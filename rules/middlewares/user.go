720459d96b4c7d587043cc97523c5abfa9b3f265
Cody <cody@zktube.io>
2022-01-25 22:16:13 +0800

    middleware rule

---
+++ rules/middlewares/user.go
@@ -0,0 +1,33 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zkTube-Labs/Toolbox/crypto/jwt"
+	"github.com/zkTube-Labs/Toolbox/message/responses"
+)
+
+type UM struct {
+	RJ *jwt.RsaJwt
+}
+
+func NewUM() *UM {
+	return &UM{
+		RJ: jwt.NewJwt(),
+	}
+}
+
+func AuthUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Token := c.GetHeader("token")
+		RJ := jwt.NewJwt()
+		User := &jwt.RJMsg{}
+		err := RJ.ParseToken(Token, User)
+		if err != nil {
+			responses.ParamErrRep(c, &responses.Responses{
+				Msg: err.Error(),
+			})
+			return
+		}
+		c.Set("UUID", User.UUID)
+	}
+}
